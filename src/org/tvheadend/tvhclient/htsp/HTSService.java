1a59659d1c08a1e17df8f717db5549eb0d6707f3
Robert Siebert <rsiebert@miroku.no-ip.com>
2015-03-11 21:13:06 +0100

    Silence two log messages

--- src/org/tvheadend/tvhclient/htsp/HTSService.java
+++ src/org/tvheadend/tvhclient/htsp/HTSService.java
@@ -809,7 +809,7 @@ public class HTSService extends Service implements HTSConnectionListener {
                     TVHClientApplication app = (TVHClientApplication) getApplication();
                     app.updateChannel(ch);
                 } catch (Throwable ex) {
-                    Log.e(TAG, "Can't load channel icon", ex);
+                    //Log.e(TAG, "Can't load channel icon", ex);
                 }
             }
         });
@@ -823,7 +823,7 @@ public class HTSService extends Service implements HTSConnectionListener {
                     TVHClientApplication app = (TVHClientApplication) getApplication();
                     app.updateChannelTag(tag);
                 } catch (Throwable ex) {
-                    Log.e(TAG, "Can't load tag icon", ex);
+                    //Log.e(TAG, "Can't load tag icon", ex);
                 }
             }
         });
